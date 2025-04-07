3725ee52eef2a59e4cb714d6b0378d344ee37c8a
KianiDev <mohammadamin.k1390@gmail.com>
2025-04-07 22:06:22 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+
 @bot.command(name='hello')
 async def hello(ctx):
     await ctx.send(f'Hello {ctx.author.mention}!')
